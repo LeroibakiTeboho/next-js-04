20eaaba178fe7a8535977bbc9db1b4a9c4a04478
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-03 15:36:56 +0200

    context provider

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -4,27 +4,37 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "../components/theme-provider";
 // import "./ImageSlider.css";
 
 function ClientRoutePage() {
   console.log("Client route rendered");
+
+  const theme = useTheme();
   return (
-    <div className="image-slider-container">
-      <Slider>
-        <div>
-          <img src="https://picsum.photos/400/200" alt="placeholder" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/300" alt="placeholder" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/250" alt="placeholder" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/350" alt="placeholder" />
-        </div>
-      </Slider>
-    </div>
+    // <div className="image-slider-container">
+    //   <Slider>
+    //     <div>
+    //       <img src="https://picsum.photos/400/200" alt="placeholder" />
+    //     </div>
+    //     <div>
+    //       <img src="https://picsum.photos/400/300" alt="placeholder" />
+    //     </div>
+    //     <div>
+    //       <img src="https://picsum.photos/400/250" alt="placeholder" />
+    //     </div>
+    //     <div>
+    //       <img src="https://picsum.photos/400/350" alt="placeholder" />
+    //     </div>
+    //   </Slider>
+    // </div>
+
+    <h1 style={{
+      color: theme.colors.secondary,
+
+    }}>
+      Client route
+    </h1>
   );
 }
 
---
+++ app/components/theme-provider.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { createContext, useContext } from "react";
+
+type Theme = {
+    colors: {
+      primary: string;
+      secondary: string;
+    };
+  };
+  
+  const defaultTheme: Theme = {
+    colors: {
+      primary: "#007bff",
+      secondary: "#6c757d",
+    },
+  };
+  
+  const ThemeContent = createContext<Theme>(defaultTheme);
+
+  
+  export const ThemeProvider = ({children}: {
+    children: React.ReactNode
+  }) => {
+    return (
+      <ThemeContent.Provider value={defaultTheme}>
+        {children}
+      </ThemeContent.Provider>
+    )
+  }
+  
+  export const useTheme = () => useContext(ThemeContent) 
