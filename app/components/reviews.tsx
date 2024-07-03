704982afa846e7d420f891c61f6bad6739cd6d4c
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-03 14:08:28 +0200

    streaming

---
+++ app/components/reviews.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Reviews = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+  return (
+    <div>Reviews</div>
+  )
+}
+
+export default Reviews
---
+++ app/product-detail/page.tsx
@@ -0,0 +1,19 @@
+import Product from "@/app/components/product";
+import Reviews from "@/app/components/reviews";
+import React, { Suspense } from "react";
+
+function ProductDetailPage() {
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+      <Suspense fallback={<p>Loading product details....</p>}>
+        <Product />
+      </Suspense>
+      <Suspense fallback={<p>Loading product reviews....</p>}>
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
+
+export default ProductDetailPage;
