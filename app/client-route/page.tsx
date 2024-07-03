a1546fe51b724f03eb1e68dead5437bc29cc2371
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-03 15:54:30 +0200

    client only code

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import React from "react";
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "../components/theme-provider";
+import { clientSideFuction } from "../utils/client-utils";
 // import "./ImageSlider.css";
 
 function ClientRoutePage() {
   console.log("Client route rendered");
+
+  const result = clientSideFuction();
 
   const theme = useTheme();
   return (
@@ -29,12 +31,16 @@ function ClientRoutePage() {
     //   </Slider>
     // </div>
 
-    <h1 style={{
-      color: theme.colors.secondary,
-
-    }}>
-      Client route
-    </h1>
+    <>
+      <h1
+        style={{
+          color: theme.colors.primary,
+        }}
+      >
+        Client route:
+      </h1>
+      {result}
+    </>
   );
 }
 
---
+++ app/utils/client-utils.ts
@@ -0,0 +1,9 @@
+import "client-only"
+
+export const clientSideFuction = () => {
+  console.log(
+    `use window object,
+        use localStorage`
+  );
+  return "client results";
+};
